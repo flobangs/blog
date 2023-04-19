40dc53d9ffb89b1869c59e71735e15a0bffa296b
flobangs <flobangs.korea@gmail.com>
2023-04-20 03:05:00 +0900

    add config/SecutiryConfig.java

---
+++ src/main/java/com/example/blog2/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.blog2.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 1. CSRF 해제 (개발할 때는 해제 - POST 맨을 사용할 수 없다)
+        // 필요할 때 지우면 된다.
+        http.csrf().disable();
+
+        // 2. Form 로그인 설정
+        http.formLogin()
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login")
+                .successHandler((request, response, authentication) -> {
+                    log.debug("디버그 : 로그인 성공");
+                })
+                .failureHandler((request, response, exception) -> {
+                    log.debug("디버그 : 로그인 실패" + exception.getMessage());
+                });
+
+        // 3. 인증, 권한 필터 설정
+        // 주소가 s로 시작하지 않으면 들어갈 수 없다.
+        http.authorizeRequests(
+                authorize -> authorize.antMatchers("/s/**")
+                        .authenticated()
+        );
+
+        return http.build();
+    }
+
+}
